9a15ee5ecb90e88ab5710480cca5f9357d4b1333
USER <USER@192.168.1.7>
2021-11-10 21:14:04 -0500

    estacion y movimiento

---
+++ src/main/java/Programa/Movimiento.java
@@ -0,0 +1,75 @@
+
+import java.util.Date;
+
+public class Movimiento {
+    private int codigo;
+    private String tipo;
+    private Date fecha;
+    private float monto;
+    private Estacion estacion;
+
+    //constructor
+    public Movimiento(int codigo, String tipo, Date fecha, float monto, Estacion estacion) {
+        this.codigo = codigo;
+        this.tipo = tipo;
+        this.fecha = fecha;
+        this.monto = monto;
+        this.estacion = estacion;
+    }
+
+    //getters
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public float getMonto() {
+        return monto;
+    }
+
+    public Estacion getEstacion() {
+        return estacion;
+    }
+
+    //setters
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public void setMonto(float monto) {
+        this.monto = monto;
+    }
+
+    public void setEstacion(Estacion estacion) {
+        this.estacion = estacion;
+    }
+
+    //toString
+    @Override
+    public String toString() {
+        return "Movimiento{" +
+                "codigo=" + codigo +
+                ", tipo='" + tipo + '\'' +
+                ", fecha=" + fecha +
+                ", monto=" + monto +
+                ", estacion=" + estacion +
+                '}';
+    }
+
+}
